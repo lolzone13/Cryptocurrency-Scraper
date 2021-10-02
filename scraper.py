7011943845132f4abcf4828207b16894a8651e81
lolzone13 <mohitscience123@gmail.com>
2021-10-02 14:35:01 +0530

    merged app.py and scraper.py

--- scraper.py
+++ scraper.py
@@ -3,51 +3,47 @@ import requests
 import pandas as pd
 import sqlite3 as sql
 
-url = 'https://goldprice.org/cryptocurrency-price'
-
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'
-}
-
-req = requests.get(url, headers=headers)
 
 
+def crypto_scraper():
 
-soup = BeautifulSoup(req.text, 'html.parser')
-table_elements = []
-
-for child in soup.find_all("table")[0].children:
-    table_elements+=[child]
-
-
-l2 = []
-j = 0
-for tr in table_elements[3].children:
-    if (j%2):
-        l2+=[tr]
-    j+=1
+    url = 'https://goldprice.org/cryptocurrency-price'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'
+    }
+    req = requests.get(url, headers=headers)
 
 
-final_data = []
-for i in l2:
+
+    soup = BeautifulSoup(req.text, 'html.parser')
+    table_elements = []
+
+    for child in soup.find_all("table")[0].children:
+        table_elements+=[child]
+
+    l2 = []
     j = 0
-    temp = []
-    for child in i.find_all("td"):
-        if (j == 7):
-            break
-        temp+=[child.text.strip()]
-        j+=1        
-    final_data+=[temp]
+    for tr in table_elements[3].children:
+        if (j%2):
+            l2+=[tr]
+        j+=1
 
 
-table_headers = ["Rank", "CryptoCurrency", "Market Cap", "Price", "Circulating Supply", "Volume(24h)", "Change(24h)"]
+    final_data = []
+    for i in l2:
+        j = 0
+        temp = []
+        for child in i.find_all("td"):
+            if (j == 7):
+                break
+            temp+=[child.text.strip()]
+            j+=1        
+        final_data+=[temp]
 
-print(final_data)
-# df = pd.DataFrame(final_data, columns=table_headers)
-# print(df.head)
+
+    # table_headers = ["Rank", "CryptoCurrency", "Market Cap", "Price", "Circulating Supply", "Volume(24h)", "Change(24h)"]
+    return final_data
 
 
-# conn = sql.connect('cryptodata.db')
-# df.to_sql('weather', conn)
 
 
